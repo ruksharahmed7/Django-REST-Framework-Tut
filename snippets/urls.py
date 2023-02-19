ee36f251c5b17808709eff5c109edca10eca93e6
rukshar69 <ruksharalam7@gmail.com>
2023-02-19 22:36:37 +0600

    first commit

---
+++ snippets/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', include(router.urls)),
+]
---
+++ test_jwt.sh
@@ -0,0 +1,5 @@
+curl \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "rukshar", "password": "12345"}' \
+  http://localhost:8000/api/token/
